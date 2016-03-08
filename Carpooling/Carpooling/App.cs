72fe313f2ed17764029e5b8303c7ac59d2315b0a
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-03-08 17:35:03 -0600

    Login
    Added Login view

--- Carpooling/Carpooling/App.cs
+++ Carpooling/Carpooling/App.cs
@@ -12,17 +12,8 @@ namespace Carpooling
 		public App ()
 		{
 			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							XAlign = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
+            //MainPage = new
+            MainPage = new Login();
 		}
 
 		protected override void OnStart ()
