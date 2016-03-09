f1ef174e8b0c57ab73e2a2622fdb81e975706593
Luis Manuel Rodriguez Scio PC <lwe_@hotmail.com>
2016-03-09 15:54:03 -0600

    Login View
    Components vars

--- Carpooling/Carpooling/Login.xaml.cs
+++ Carpooling/Carpooling/Login.xaml.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 
 namespace Carpooling
 {
-	public partial class Login : ContentPage
+	public partial class Login 
 	{
 		public Login ()
 		{
 			InitializeComponent ();
+
 		}
+
+        void SignIn(Object sender, EventArgs e)
+        {
+            
+            
+        }
 	}
 }
